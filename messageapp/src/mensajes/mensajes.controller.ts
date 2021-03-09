63ca639141f7a779fc2989f97ab74022ebd29c74
Silviu <silviu.manea.hamzau@outlook.com>
2021-03-09 11:06:58 +0100

    ADD-INyección de dependencias en controller

--- messageapp/src/mensajes/mensajes.controller.ts
+++ messageapp/src/mensajes/mensajes.controller.ts
@@ -1,25 +1,70 @@
+import { HttpStatus, Param, Res } from '@nestjs/common';
 import { Body, Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { response } from 'express';
 import { CreateMensajeDto } from './dto/create-mensaje-dto';
+import { MensajesService } from './mensajes.service';
 
 @Controller('mensajes')
 export class MensajesController {
+  constructor(private mensajesServices: MensajesService) {}
+
   @Post()
-  create(@Body() createMensajeDto: CreateMensajeDto) {
-    return 'mensaje creado';
+  create(@Body() createMensajeDto: CreateMensajeDto, @Res() response) {
+    this.mensajesServices
+      .createMensaje(createMensajeDto)
+      .then((mensaje) => {
+        response.status(HttpStatus.CREATED).json(mensaje);
+      })
+      .catch(() => {
+        response
+          .status(HttpStatus.FORBIDDEN)
+          .json({ mensaje: 'error en la creación del mensaje.' });
+      });
   }
 
   @Get()
-  getAll() {
-    return ' lista de mensajes';
+  getAll(@Res() response) {
+    this.mensajesServices
+      .getAll()
+      .then((mensajesList) => {
+        response.status(HttpStatus.OK).json(mensajesList);
+      })
+      .catch(() => {
+        response
+          .status(HttpStatus.FORBIDDEN)
+          .json({ mensaje: 'error en la obtención de mensajes.' });
+      });
   }
 
   @Put(':id')
-  update(@Body() updateMensajeDto: CreateMensajeDto) {
-    return 'mensaje actualizado';
+  update(
+    @Body() updateMensajeDto: CreateMensajeDto,
+    @Res() response,
+    @Param('id') idMensaje,
+  ) {
+    this.mensajesServices
+      .updateMensaje(idMensaje, updateMensajeDto)
+      .then((mensaje) => {
+        response.status(HttpStatus.OK).json(mensaje);
+      })
+      .catch(() => {
+        response
+          .status(HttpStatus.FORBIDDEN)
+          .json({ mensaje: 'error en la edición de mensajes.' });
+      });
   }
 
   @Delete(':id')
-  delete() {
-    return 'mensaje eliminado';
+  delete(@Res() response, @Param('id') idMensaje) {
+    this.mensajesServices
+      .deleteMensaje(idMensaje)
+      .then((res) => {
+        response.status(HttpStatus.OK).json(res);
+      })
+      .catch(() => {
+        response
+          .status(HttpStatus.FORBIDDEN)
+          .json({ mensaje: 'error en la eliminación de mensajes.' });
+      });
   }
 }
